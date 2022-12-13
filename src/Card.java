6f8145944db4fc4e9295dc22dd8cd3c64959933a
ElitaKuld <elitakuld@gmail.com>
2022-12-13 12:24:28 +0100

    Skapat klasser Card och SetOfCards

---
+++ src/Card.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Card extends JPanel implements Flippable{
+
+    private String value;
+
+    public Card( String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void flipACard(Card card){
+
+    }
+}
---
+++ src/SetOfCards.java
@@ -0,0 +1,35 @@
+
+import java.util.List;
+
+public class SetOfCards {
+
+    Card cardA = new Card("A");
+    Card cardB = new Card("B");
+    Card cardC = new Card("C");
+    Card cardD = new Card("D");
+    Card cardE = new Card("E");
+    Card cardF = new Card("F");
+    Card cardG = new Card("G");
+    Card cardH = new Card("H");
+    Card cardI = new Card("I");
+    Card cardJ = new Card("J");
+    Card cardK = new Card("K");
+    Card cardL = new Card("L");
+    Card cardM = new Card("M");
+    Card cardN = new Card("N");
+    Card cardO = new Card("O");
+    Card cardP = new Card("P");
+    Card cardQ = new Card("Q");
+    Card cardR = new Card("R");
+    Card cardS = new Card("S");
+    Card cardT = new Card("T");
+    Card cardU = new Card("U");
+    Card cardV = new Card("V");
+    Card cardW = new Card("W");
+    Card cardX = new Card("X");
+    Card cardY = new Card("Y");
+    Card cardZ = new Card("Z");
+
+    List<Card> listOfCards = List.of(cardA, cardB, cardC, cardD, cardE, cardF, cardG, cardH, cardI, cardJ, cardK,
+            cardL, cardM, cardN, cardO, cardP, cardQ, cardR, cardS, cardT, cardU, cardV, cardW, cardX, cardY, cardZ);
+}
