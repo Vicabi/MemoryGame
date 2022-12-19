5623407e6abb17f18708939526c3171776a89717
ElitaKuld <elitakuld@gmail.com>
2022-12-19 11:26:31 +0100

    En sista pushning innan mergningen med master

--- src/Settings.java
+++ src/Settings.java
@@ -95,8 +95,6 @@ public class Settings extends JFrame implements ActionListener {
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(500, 500);
-
-
     }
 
     @Override
